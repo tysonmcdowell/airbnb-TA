79f407d4517f9a95ed5d270789ae149655995870
tysonmcdowell <tyson.mcdowell06@gmail.com>
2024-12-17 15:14:31 -0600

    help adil pleaseeee

--- backend/db/migrations/20241217164058-create-spots.js
+++ backend/db/migrations/20241217164058-create-spots.js
@@ -8,7 +8,7 @@ if (process.env.NODE_ENV === "production") {
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable(
-      "SpotImages",
+      "Spots",
       {
         id: {
           allowNull: false,
